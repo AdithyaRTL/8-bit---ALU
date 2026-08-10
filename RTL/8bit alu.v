ea4c863b35444607d076fb95b9e033f0526715e2
AdithyaRTL <adithyabhushan18@gmail.com>
2026-08-10 23:08:24 +0530

    8bit alu.v

--- RTL/8bit alu.v
+++ RTL/8bit alu.v
@@ -1,5 +1,8 @@
 module alu_8bit (
-  input [7:0] a;
-  input [7:0] b;
-  input [3:0] alu_sel;
+  input wire [7:0] a;
+  input wire [7:0] b;
+  input wire [3:0] alu_sel;
+  output reg [7:0] alu_out;
+  output reg carry_out;
+  output reg zero;
   
